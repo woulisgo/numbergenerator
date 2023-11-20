fbe9364ef68e82fb881984f8b0329f1aeca33a0f
Oliver Hašler <1190@student.itgymnazium.cz>
2023-11-20 12:38:38 +0100

    numbergenerator

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int userInput1 = 25;
+int userInput2 = 12;
+int userInput3 = 3;
+/*
+ * 12
+ * 13
+ * 14
+ * ...
+ * do 25
+ */
+for (int i = userInput1; i < userInput2; i += userInput3)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("napiš slovo");
+string slovo = Console.ReadLine();
+for (int i2 = 0; i2 <= 5; i2++)
+{
+    Console.WriteLine(i2);
+}
